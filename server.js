98b289c87c369f675d0ade9924bb0cde6e424a0b
Landry Nsengiyumva <57137305+landry5@users.noreply.github.com>
2024-12-19 03:00:07 +0000

    Update server.js typo

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 import express from "express";
 import cors from "cors";
 import records from "./routes/record.js";
-import multer from "multer";import express from "express";
+import multer from "multer";
 import cors from "cors";
 import records from "./routes/record.js";
 import multer from "multer";
